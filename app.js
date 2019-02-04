5645783bafdf1684a7c07fd63e950068da05b62d
Julio Quevedo <juliovquevedo@gmail.com>
2019-02-04 13:25:40 -0500

    First step in making the moves cells 12 and 15 move correctly when cell 16 is empty

--- app.js
+++ app.js
@@ -1,38 +1,65 @@
 window.onload = function() {
 var cells = [
-    {pos: "11", movable: false, clear: false, top: "0.2%", left: "0.2%"},
-    {pos: "12", movable: false, clear: false, top: "0.2%", left: "25.2%"},
-    {pos: "13", movable: false, clear: false, top: "0.2%", left: "50.2%"},
-    {pos: "14", movable: false, clear: false, top: "0.2%", left: "75.2%"},
-    {pos: "21", movable: false, clear: false, top: "25.2%", left: "0.2%"},
-    {pos: "22", movable: false, clear: false, top: "25.2%", left: "25.2%"},
-    {pos: "23", movable: false, clear: false, top: "25.2%", left: "50.2%"},
-    {pos: "24", movable: false, clear: false, top: "25.2%", left: "75.2%"},
-    {pos: "31", movable: false, clear: false, top: "50.2%", left: "0.2%"},
-    {pos: "32", movable: false, clear: false, top: "50.2%", left: "25.2%"},
-    {pos: "33", movable: true, clear: false, top: "50.2%", left: "50.2%"},
-    {pos: "34", movable: false, clear: false, top: "50.2%", left: "75.2%"},
-    {pos: "41", movable: false, clear: false, top: "75.2%", left: "0.2%"},
-    {pos: "42", movable: false, clear: false, top: "75.2%", left: "25.2%"},
-    {pos: "43", movable: true, clear: false, top: "75.2%", left: "50.2%"},
-    {pos: "44", movable: false, clear: true, top: "75.2%", left: "75.2%"},
+    {pos: "p11", movable: false, clear: false, top: "0.2%", left: "0.2%"},
+    {pos: "p12", movable: false, clear: false, top: "0.2%", left: "25.2%"},
+    {pos: "p13", movable: false, clear: false, top: "0.2%", left: "50.2%"},
+    {pos: "p14", movable: false, clear: false, top: "0.2%", left: "75.2%"},
+    {pos: "p21", movable: false, clear: false, top: "25.2%", left: "0.2%"},
+    {pos: "p22", movable: false, clear: false, top: "25.2%", left: "25.2%"},
+    {pos: "p23", movable: false, clear: false, top: "25.2%", left: "50.2%"},
+    {pos: "p24", movable: false, clear: false, top: "25.2%", left: "75.2%"},
+    {pos: "p31", movable: false, clear: false, top: "50.2%", left: "0.2%"},
+    {pos: "p32", movable: false, clear: false, top: "50.2%", left: "25.2%"},
+    {pos: "p33", movable: false, clear: false, top: "50.2%", left: "50.2%"},
+    {pos: "p34", movable: true, clear: false, top: "50.2%", left: "75.2%"},
+    {pos: "p41", movable: false, clear: false, top: "75.2%", left: "0.2%"},
+    {pos: "p42", movable: false, clear: false, top: "75.2%", left: "25.2%"},
+    {pos: "p43", movable: true, clear: false, top: "75.2%", left: "50.2%"},
+    {pos: "p44", movable: false, clear: true, top: "75.2%", left: "75.2%"},
 ];
 
-
-var clear = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true];
-
-function clearPos() {
-//    posClear[pos1] = true;
-//    posClear[pos2] = false;
-    console.log(clear[3]);
-    console.log(clear[15]);
+var clear = 15;
+    
+//document.getElementById("p11").onclick = function () { move(0) };
+//document.getElementById("p12").onclick = move(1);
+//document.getElementById("p13").onclick = move(2);
+//document.getElementById("p14").onclick = move(3);
+//document.getElementById("p21").onclick = move(4);
+//document.getElementById("p22").onclick = move(5);
+//document.getElementById("p23").onclick = move(6);
+//document.getElementById("p24").onclick = move(7);
+//document.getElementById("p31").onclick = move(8);
+//document.getElementById("p32").onclick = move(9);
+document.getElementById("p34").onclick = function() { 
+    move(11);
+};
+//document.getElementById("p34").onclick = move(11);
+//document.getElementById("p41").onclick = move(12);
+//document.getElementById("p42").onclick = move(13);
+document.getElementById("p43").onclick = function() {
+    move(14);
+};
+//document.getElementById("p44").onclick = move(15);
+    
+    
+function move(e) {
+    if(cells[e].movable){
+        document.getElementById(cells[e].pos).style.transition = "all 1s";
+        document.getElementById(cells[e].pos).style.top = cells[clear].top;
+        document.getElementById(cells[e].pos).style.left = cells[clear].left;
+    }
 }
 
-clearPos();
-var x = document.getElementById("p11").style.top;
-console.log(x);
-console.log("aaa"+x+"bbb");
-console.log(typeof(x));
-document.getElementById("p11").style.top = "1.4%";
-console.log(x);
+    
+    
+    
+    
+    
+//    document.getElementById("p43").onclick = function() {
+//        console.log("i");
+////        document.getElementById("p43").style.transition = "top 1s";
+////        document.getElementById("p43").style.top = "75.2%";
+//        document.getElementById("p43").style.transition = "all 1s";
+//        document.getElementById("p43").style.left = "75.2%";
+//    }
 }
