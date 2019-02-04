1792bd58cdf0dcdf9466193f7ec48e8e59586a08
Julio Quevedo <juliovquevedo@gmail.com>
2019-02-04 16:22:01 -0500

    Added unset and reset functions

--- app.js
+++ app.js
@@ -20,33 +20,226 @@ var cells = [
 
 var clear = 15;
     
-//document.getElementById("p11").onclick = function () { move(0) };
-//document.getElementById("p12").onclick = move(1);
-//document.getElementById("p13").onclick = move(2);
-//document.getElementById("p14").onclick = move(3);
-//document.getElementById("p21").onclick = move(4);
-//document.getElementById("p22").onclick = move(5);
-//document.getElementById("p23").onclick = move(6);
-//document.getElementById("p24").onclick = move(7);
-//document.getElementById("p31").onclick = move(8);
-//document.getElementById("p32").onclick = move(9);
-document.getElementById("p34").onclick = function() { 
-    move(11);
-};
-//document.getElementById("p34").onclick = move(11);
-//document.getElementById("p41").onclick = move(12);
-//document.getElementById("p42").onclick = move(13);
-document.getElementById("p43").onclick = function() {
-    move(14);
-};
-//document.getElementById("p44").onclick = move(15);
+document.getElementById("p11").onclick = function() { move(0) };
+document.getElementById("p12").onclick = function() { move(1) };
+document.getElementById("p13").onclick = function() { move(2) };
+document.getElementById("p14").onclick = function() { move(3) };
+document.getElementById("p21").onclick = function() { move(4) };
+document.getElementById("p22").onclick = function() { move(5) };
+document.getElementById("p23").onclick = function() { move(6) };
+document.getElementById("p24").onclick = function() { move(7) };
+document.getElementById("p31").onclick = function() { move(8) };
+document.getElementById("p32").onclick = function() { move(9) };
+document.getElementById("p33").onclick = function() { move(10) };
+document.getElementById("p34").onclick = function() { move(11) };
+document.getElementById("p41").onclick = function() { move(12) };
+document.getElementById("p42").onclick = function() { move(13) };
+document.getElementById("p43").onclick = function() { move(14) };
+document.getElementById("p44").onclick = function() { move(15) };
     
     
 function move(e) {
     if(cells[e].movable){
-        document.getElementById(cells[e].pos).style.transition = "all 1s";
+        document.getElementById(cells[e].pos).style.transition = "all 0.4s";
         document.getElementById(cells[e].pos).style.top = cells[clear].top;
         document.getElementById(cells[e].pos).style.left = cells[clear].left;
+        
+        unset(cells[clear].pos);
+        var tempClear = clear;
+        clear = e;
+        reset(cells[clear].pos);
+        
+        var temp = cells[e].pos;
+        cells[e].pos = cells[tempClear].pos;
+        cells[tempClear].pos = temp;
+        
+        cells[e].clear = true;
+    }
+}
+    
+function unset(p) {
+    switch(p){
+        case "p11":
+            cells[1].movable = false;
+            cells[4].movable = false;
+            cells[0].clear = false;
+            break;
+        case "p12":
+            cells[0].movable = false;
+            cells[2].movable = false;
+            cells[5].movable = false;
+            cells[1].clear = false;
+            break;
+        case "p13":
+            cells[1].movable = false;
+            cells[3].movable = false;
+            cells[6].movable = false;
+            cells[2].clear = false;
+            break;
+        case "p14":
+            cells[2].movable = false;
+            cells[7].movable = false;
+            cells[3].clear = false;
+            break;
+        case "p21":
+            cells[0].movable = false;
+            cells[5].movable = false;
+            cells[8].movable = false;
+            cells[4].clear = false;
+            break;
+        case "p22":
+            cells[1].movable = false;
+            cells[4].movable = false;
+            cells[6].movable = false;
+            cells[9].movable = false;
+            cells[5].clear = false;
+            break;
+        case "p23":
+            cells[2].movable = false;
+            cells[5].movable = false;
+            cells[7].movable = false;
+            cells[10].movable = false;
+            cells[6].clear = false;
+            break;
+        case "p24":
+            cells[3].movable = false;
+            cells[6].movable = false;
+            cells[11].movable = false;
+            cells[7].clear = false;
+            break;
+        case "p31":
+            cells[4].movable = false;
+            cells[9].movable = false;
+            cells[12].movable = false;
+            cells[8].clear = false;
+            break;
+        case "p32":
+            cells[5].movable = false;
+            cells[8].movable = false;
+            cells[10].movable = false;
+            cells[13].movable = false;
+            cells[9].clear = false;
+            break;
+        case "p33":
+            cells[6].movable = false;
+            cells[9].movable = false;
+            cells[11].movable = false;
+            cells[14].movable = false;
+            cells[10].clear = false;
+            break;
+        case "p34":
+            cells[7].movable = false;
+            cells[10].movable = false;
+            cells[15].movable = false;
+            cells[11].clear = false;
+            break;
+        case "p41":
+            cells[8].movable = false;
+            cells[13].movable = false;
+            cells[12].clear = false;
+            break;
+        case "p42":
+            cells[9].movable = false;
+            cells[12].movable = false;
+            cells[14].movable = false;
+            cells[13].clear = false;
+            break;
+        case "p43":
+            cells[10].movable = false;
+            cells[13].movable = false;
+            cells[15].movable = false;
+            cells[8].clear = false;
+            break;
+        case "p44":
+            cells[11].movable = false;
+            cells[14].movable = false;
+            cells[15].clear = false;
+            break;
+    }
+}
+    
+function reset(p) {
+    switch(p){
+        case "p11":
+            cells[1].movable = true;
+            cells[4].movable = true;
+            break;
+        case "p12":
+            cells[0].movable = true;
+            cells[2].movable = true;
+            cells[5].movable = true;
+            break;
+        case "p13":
+            cells[1].movable = true;
+            cells[3].movable = true;
+            cells[6].movable = true;
+            break;
+        case "p14":
+            cells[2].movable = true;
+            cells[7].movable = true;
+            break;
+        case "p21":
+            cells[0].movable = true;
+            cells[5].movable = true;
+            cells[8].movable = true;
+            break;
+        case "p22":
+            cells[1].movable = true;
+            cells[4].movable = true;
+            cells[6].movable = true;
+            cells[9].movable = true;
+            break;
+        case "p23":
+            cells[2].movable = true;
+            cells[5].movable = true;
+            cells[7].movable = true;
+            cells[10].movable = true;
+            break;
+        case "p24":
+            cells[3].movable = true;
+            cells[6].movable = true;
+            cells[11].movable = true;
+            break;
+        case "p31":
+            cells[4].movable = true;
+            cells[9].movable = true;
+            cells[12].movable = true;
+            break;
+        case "p32":
+            cells[5].movable = true;
+            cells[8].movable = true;
+            cells[10].movable = true;
+            cells[13].movable = true;
+            break;
+        case "p33":
+            cells[6].movable = true;
+            cells[9].movable = true;
+            cells[11].movable = true;
+            cells[14].movable = true;
+            break;
+        case "p34":
+            cells[7].movable = true;
+            cells[10].movable = true;
+            cells[15].movable = true;
+            break;
+        case "p41":
+            cells[8].movable = true;
+            cells[13].movable = true;
+            break;
+        case "p42":
+            cells[9].movable = true;
+            cells[12].movable = true;
+            cells[14].movable = true;
+            break;
+        case "p43":
+            cells[10].movable = true;
+            cells[13].movable = true;
+            cells[15].movable = true;
+            break;
+        case "p44":
+            cells[11].movable = true;
+            cells[14].movable = true;
+            break;
     }
 }
 
